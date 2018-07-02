bb3c5e730464574af8205ff8fe9b22272ed0ca9f
theboymo <billfero@gmail.com>
2018-07-02 19:22:21 +0100

    add password digest to user model

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_165659) do
+ActiveRecord::Schema.define(version: 2018_07_02_181037) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.datetime "last_login"
   end
 
 end
