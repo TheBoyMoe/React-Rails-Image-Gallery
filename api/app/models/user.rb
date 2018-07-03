9a42eef97372491996bc439fb7ccd969323adfd2
theboymo <billfero@gmail.com>
2018-07-03 09:06:22 +0100

    ensure all email addresses asre lowercase, add first and last name methods

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   validates_presence_of :password_digest
   validates_length_of :password, maximum: 72, minimum: 8
+
+  before_validation {
+    self.email = self.email.to_s.downcase
+  }
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
