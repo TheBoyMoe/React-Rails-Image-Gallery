6012ec4c9785ddae5d06896089e696c91b042770
theboymo <billfero@gmail.com>
2018-07-03 09:26:33 +0100

    add create action to users controller

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,28 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authenticate_user, only: %i[index, current]  
 
       def index
-        @users = User.all
-        render json: @users
+        render json: { status: 200, msg: 'User logged in'}
       end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: { status: 200, msg: 'User was created'}
+        end
+      end
+
+      def current
+        current_user.update!(last_login: Time.now)
+        render json: current_user
+      end
+
+      private
+        def user_params
+          params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
     end
   end
 end
