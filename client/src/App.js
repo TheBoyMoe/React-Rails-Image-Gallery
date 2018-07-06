b768ef58ec395b6e55ef4bc3948d32ce20942d90
theboymo <billfero@gmail.com>
2018-07-06 15:18:05 +0100

    configure an implement redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Toolbar from './components/toolbar/Toolbar';
 import AppRouter from './components/router/AppRouter';
+import configureStore from './store/store';
+
 import './styles/styles.css';
+
+const appStore = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Toolbar />
-          <AppRouter />
-        </div>
-      </Router>
+      <Provider store={ appStore }>
+        <Router>
+          <div>
+            <Toolbar />
+            <AppRouter />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
