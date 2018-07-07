9bd30690b1081bdb4c6512369c6f63799181c326
theboymo <billfero@gmail.com>
2018-07-07 17:42:44 +0100

    create gallery, image and paperclip migrations and create models

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_181037) do
+ActiveRecord::Schema.define(version: 2018_07_07_161737) do
+
+  create_table "galleries", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.integer "gallery_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "file_file_name"
+    t.string "file_content_type"
+    t.integer "file_file_size"
+    t.datetime "file_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
