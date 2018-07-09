5903a06214446bf49c4ebb367600ee5ebe930bc4
theboymo <billfero@gmail.com>
2018-07-09 19:15:16 +0100

    create a banner component to display a background image

---
+++ client/src/components/ui/banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const banner = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <p className="lead">View the latest additions to our collection.</p>
+      </div>
+    </div>
+  );
+};
+export default banner;
