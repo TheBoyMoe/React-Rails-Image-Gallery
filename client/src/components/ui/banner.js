f411141f5ce9a1aba22fc06b00b8722786a0c67b
theboymo <billfero@gmail.com>
2018-07-10 12:57:46 +0100

    add background image to banner

--- client/src/components/ui/banner.js
+++ client/src/components/ui/banner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const banner = () => {
   return (
     <div className="jumbotron jumbotron-fluid">
-      <div className="container">
+      <div className="screen">
         <p className="lead">View the latest additions to our collection.</p>
       </div>
     </div>
