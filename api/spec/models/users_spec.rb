86e723bb267e12bdf6c94ba3ef42efe51b923471
theboymo <billfero@gmail.com>
2018-07-17 09:51:17 +0100

    validate first/last_name methods and max length of password

--- api/spec/models/users_spec.rb
+++ api/spec/models/users_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
-  # it { should validate_uniqueness_of(:email) }
   it { should validate_presence_of(:password_digest) }
 
   it 'is not valid if the password is less than 8 characters in length' do
@@ -11,9 +10,45 @@ RSpec.describe User, type: :model do
     expect(user).to_not be_valid
   end
 
+  it 'is not valid if the password is greater than 72 characters in length' do
+    password = 'a' * 73
+    user = User.new(name: 'Tom', email: 'tom@ex.com', password: password)
+    expect(user).to_not be_valid
+  end
+
   it 'should validate the uniqueness of email' do
     User.create(name: 'Tom', email: 'tom@ex.com', password: '12345678')
     user = User.new(name: 'Tom', email: 'TOM@ex.com', password: '12345678')
     expect(user).to_not be_valid
+  end
+
+  it 'User has many galleries' do
+    user = User.create(name: 'Tom', email: 'tom@ex.com', password: '12345678')
+    gallery = Gallery.create(title: 'Test Gallery', user: user)
+    expect(user.galleries).to include(gallery)
+  end
+
+  describe '#first_name returns' do
+    it 'the first name when full name provided' do
+      user = User.create(name: 'Tom Jones', email: 'tom@ex.com', password: '12345678')
+      expect(user.first_name).to eq('Tom')
+    end
+
+    it 'the name when a single word string is provided' do
+      user = User.create(name: 'Tom', email: 'tom@ex.com', password: '12345678')
+      expect(user.first_name).to eq('Tom')
+    end
+  end
+  
+  describe '#last_name returns' do
+    it 'the last name when the full name is provided' do
+      user = User.create(name: 'Tom Jones', email: 'tom@ex.com', password: '12345678')
+      expect(user.last_name).to eq('Jones')
+    end
+    
+    it 'the name when a single word string is provided' do
+      user = User.create(name: 'Tom', email: 'tom@ex.com', password: '12345678')
+      expect(user.last_name).to eq('Tom')
+    end
   end
 end
