4d524dca27ee4e198ea00ea30f05f567865f6d94
theboymo <billfero@gmail.com>
2018-07-24 16:45:10 +0100

    amend status code returned by user create action

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -3,14 +3,14 @@ module Api
     class UsersController < ApplicationController
       before_action :authenticate_user, only: %i[index current]
 
-      def index
-        render json: { status: 200, msg: 'User logged in'}
-      end
+      # def index
+      #   render status: 200, json: {  messsage: 'User logged in'}
+      # end
 
       def create
         @user = User.new(user_params)
         if @user.save
-          render json: { status: 200, msg: 'User was created'}
+          render status: 201, json: {  messsage: 'User was created'}
         end
       end
 
